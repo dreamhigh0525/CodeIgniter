dreamhigh0525/CodeIgniter

commit 88cb278a1e52dd7db5b0ebe2037c12f0dd69c0c1

Fix issue #1456

system/database/DB_query_builder.php
@@ -953,7 +953,9 @@ abstract class CI_DB_query_builder extends CI_DB_driver {
 				$part = trim($part);
 				if ( ! in_array($part, $this->qb_aliased_tables))
 				{
-					$part = $this->protect_identifiers(trim($part));
+					$part = preg_match('/^(.+)\s+(ASC|DESC)$/i', $part, $matches)
+						? $this->protect_identifiers(rtrim($matches[1])).' '.$matches[2]
+						: $this->protect_identifiers($part);
 				}
 
 				$temp[] = $part;
@@ -963,7 +965,9 @@ abstract class CI_DB_query_builder extends CI_DB_driver {
 		}
 		elseif ($direction !== $this->_random_keyword && $escape === TRUE)
 		{
-			$orderby = $this->protect_identifiers($orderby);
+			$part = preg_match('/^(.+)\s+(ASC|DESC)$/i', $orderby, $matches)
+				? $this->protect_identifiers(rtrim($matches[1])).' '.$matches[2]
+				: $this->protect_identifiers($orderby);
 		}
 
 		$this->qb_orderby[] = $orderby_statement = $orderby.$direction;

commit 0c48379b6208c46f55439983db1e9f53c649f161

Fix an issue introduced in 88cb278a1e52dd7db5b0ebe2037c12f0dd69c0c1

system/database/DB_query_builder.php
@@ -965,7 +965,7 @@ abstract class CI_DB_query_builder extends CI_DB_driver {
 		}
 		elseif ($direction !== $this->_random_keyword && $escape === TRUE)
 		{
-			$part = preg_match('/^(.+)\s+(ASC|DESC)$/i', $orderby, $matches)
+			$orderby = preg_match('/^(.+)\s+(ASC|DESC)$/i', $orderby, $matches)
 				? $this->protect_identifiers(rtrim($matches[1])).' '.$matches[2]
 				: $this->protect_identifiers($orderby);
 		}